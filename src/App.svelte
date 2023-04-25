877205a03fb9e1b298e3dd52d53a1ba6a2f2ee2f
Phu Mai <s3927049@rmit.edu.vn>
2023-04-25 23:42:03 +0700

    first page

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,207 @@
 
 <script>
-
+import Navbar from "./Navbar.svelte";
 
 </script>
 
 <main>
+    <Navbar/>
 
-hello world
+    <div class="banner">
+
+
+    </div>
+
+    <div class="package-container">
+
+        <div class="brand-name">
+            <img src="src\assets\brand middle.png" alt="">
+        </div>
+
+        <div class="packaging">
+            <img src="src\assets\package middle.png" alt="">
+        </div>
+
+    </div>
+
+    <div class="launch"> 
+        <img src="src\assets\launch.png" alt="">
+    </div>
+
+    <div class="showcase">
+
+        <div class="pink">
+            <img src="src\assets\pink.png" alt="">
+        </div>
+
+        <div class="white">
+            <img src="src\assets\white.png" alt="">
+        </div>
+    </div>
+
+    <div class="quote"> 
+        <img src="src\assets\“Wholeheartedly made by Aoife”.png" alt="">
+    </div>
+
+    <div class="logo-container">
+        <img src="src\assets\logo.png" alt="">
+    </div>
+
+
+
 
 </main>
 
 
 <style>
+
+.banner {
+        width: 100vw;
+        height: 100vh;
+        background: url(".\assets\landing upper desktop.png") no-repeat;
+        background-size: cover;
+    }
+
+.package-container {
+    display: grid;
+    grid-template-columns: auto auto;
+    place-items: center;
+
+}
+
+.brand-name img{
+    width: 50vw;
+}
+
+.packaging img{
+    width: 50vw;
+}
+
+.launch {
+    display: flex;
+    justify-content: left;
+    margin-left: 100px;
+    margin-top: 50px;
+    margin-bottom: 50px;
+}
+
+.launch img {
+    width: 500px;
+}
+
+.showcase {
+    display: grid;
+    grid-template-columns: auto auto;
+}
+
+.pink img {
+    width: 50vw;
+    
+}
+
+.white img {
+    width: 50vw;
+}
+
+.quote {
+    margin-top: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.quote img {
+    width: 20vw;
+}
+
+.logo-container {
+    margin-top: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.logo-container img {
+    width: 20vw;
+}
+
+
+@media (max-width: 1600px){
+
+.banner {
+    background-size: contain;
+}
+}
+
+
+
+@media (max-width: 960px){
+
+    .banner {
+        width: 100vw;
+        background: url(".\assets\landing upper.png") no-repeat;
+        background-size: contain;
+    }
+
+    .package-container {
+        grid-template-columns: auto;
+    
+}
+
+.brand-name img{
+    width: 100vw;
+}
+
+.packaging img{
+    width: 100vw;
+}
+
+.launch {
+    justify-content: right;
+    margin-left: 0px;
+    margin-right: 50px;
+    margin-top: 25px;
+    margin-bottom: 25px;
+}
+
+.launch img {
+    width: 200px;
+}
+
+.showcase {
+    grid-template-columns: auto ;
+}
+
+.pink img {
+    width: 100vw;
+    
+}
+
+.white img {
+    margin-top: -10px;
+    width: 100vw;
+}
+
+.quote img {
+    width: 30vw;
+}
+
+
+
+
+}
+
+@media (max-width: 525px){
+    .brand-name {
+        margin-top: -100px;
+    }
+}
+
+@media (max-width: 450px){
+    .brand-name {
+        margin-top: -200px;
+    }
+}
+
 
 </style>
---
+++ src/Navbar.svelte
@@ -0,0 +1,124 @@
+<header>
+    <nav>
+  <input type="checkbox" id="menu">
+  <label for="menu" class="menu-icon">
+  <img src="src\assets\menu.png" alt="menu">
+  </label>
+
+  <label class="logo"><a href="index.html"><img src="src\assets\logo.png" alt=""></a></label>
+      <ul>
+        
+        <li><a href="category.html">ABOUT US</a></li>
+        <li><a href="contact.html">EXCHANGE POLICY</a></li>
+      </ul>
+    </nav>
+  </header>
+
+<style>
+@font-face {
+  font-family: "blooming";
+  src: url(..\src\fonts\9Slide03-IcielBlooming-Elegant-Sans1_1.ttf) format('truetype');
+}
+
+nav {
+  height: 80px;
+  width: 100%;
+  position: fixed;
+  top: 0;
+  z-index: 1000;
+  
+}
+.logo {
+    margin: auto;
+  width: 50%;
+}
+.logo a img{
+    
+margin-top: 30px;
+margin-left: 30px;
+  width: 100px;
+ 
+}
+
+.menu-icon img{
+  height: 30px;
+  width: 30px;
+}
+nav ul {
+  float: right;
+  margin-right: 100px;
+}
+
+nav ul li,
+.logo {
+  display: inline-block;
+  line-height: 80px;
+  margin: 0 5px;
+}
+
+nav ul li a{
+
+    color: black;
+  font-family: "blooming";
+  font-size: 30px;
+  text-transform: uppercase;
+  text-decoration: none;
+  padding: 10px 13px;
+  border-radius: 3px;
+}
+
+nav ul li a:hover {
+  background-color: var(--grey);
+  color: #e8e8e8;
+  transition: 0.5s;
+}
+
+.menu-icon {
+  cursor: pointer;
+  font-size: 28px;
+  float: right;
+  line-height: 80px;
+  margin-right: 40px;
+  display: none;
+}
+
+#menu {
+  display: none;
+}
+
+/* Mobile layout */
+@media (max-width: 960px) {
+    .logo a img{
+    
+    margin-top: 15px;
+    }
+
+  .menu-icon {
+    display: block;
+    }
+  nav ul {
+
+    width: 100%;
+    height: 100vh;
+    top: 80px;
+    text-align: center;
+    right: -120%;
+    transition: all .5s;
+    position: fixed;
+    background-color: #FBFBFA;
+    margin-right: 0;
+    z-index: 1;
+  }
+  nav ul li {
+    display: block;
+  }
+
+  nav ul li a {
+    padding: 10px 20vw;
+  }
+  #menu:checked ~ ul{
+    right: 0;
+  }
+
+}
+</style>
