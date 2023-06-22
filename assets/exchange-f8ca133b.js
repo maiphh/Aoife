b3f6d9afe243d79568b12d850d183e4e59974389
Phu Mai <s3927049@rmit.edu.vn>
2023-06-22 20:16:53 +0700

    Updates

---
+++ assets/exchange-f8ca133b.js
@@ -0,0 +1,37 @@
+import{S as J,i as K,s as Q,N as Z,F as ee,e as a,c as G,b as D,t as l,d as o,f as g,g as H,h as M,k as c,j as p,m as b,n as te,o as O,p as e,w as ne,r as U,u as X,v as Y}from"./Footer-7d3d6ed4.js";function re(j){let r,h,y,u,w,_,m,f,t,x,$,E,B,S,R,T,C,I,W,k,A,N,P,z,L,d,v;return h=new Z({}),d=new ee({}),{c(){r=a("main"),G(h.$$.fragment),y=D(),u=a("div"),w=l("EXCHANGE POLICY"),_=D(),m=a("div"),f=a("div"),t=a("p"),x=l(`We really understand and sympathize with your \r
+            desire to return or exchange the items as you are \r
+            unhappy upon receiving it. Therefore, we want to \r
+            make it clear here. Please help us by carefully \r
+            reading the terms and conditions below: `),$=a("br"),E=a("br"),B=l(`\r
+            \r
+                \r
+                                        \r
+            1. We do not refund or exchange to another item with a lower price after purchasing. `),S=a("br"),R=l(`\r
+                                            \r
+            2. We only support customers exchange the size of the same item or exchange to a new item if the item you received was damaged (photo and video for proving) in 3 days for online purchasing.\r
+                         `),T=a("br"),C=l(`                   \r
+            3. The item for exchange MUST BE returned in original condition with tags and labels included as well.\r
+                  `),I=a("br"),W=l(`                          \r
+            4. SALE items can not be refunded or exchanged. `),k=a("br"),A=l(`\r
+                        5. Customers must pay for the additional shipping fees for the exchanged items. Shipping fees are non-refundable.\r
+            `),N=a("br"),P=a("br"),z=l(`\r
+            \r
+            Thank you for your attention.`),L=D(),G(d.$$.fragment),this.h()},l(s){r=o(s,"MAIN",{class:!0});var i=g(r);H(h.$$.fragment,i),y=M(i),u=o(i,"DIV",{class:!0});var V=g(u);w=c(V,"EXCHANGE POLICY"),V.forEach(p),_=M(i),m=o(i,"DIV",{class:!0});var q=g(m);f=o(q,"DIV",{class:!0});var F=g(f);t=o(F,"P",{class:!0});var n=g(t);x=c(n,`We really understand and sympathize with your \r
+            desire to return or exchange the items as you are \r
+            unhappy upon receiving it. Therefore, we want to \r
+            make it clear here. Please help us by carefully \r
+            reading the terms and conditions below: `),$=o(n,"BR",{}),E=o(n,"BR",{}),B=c(n,`\r
+            \r
+                \r
+                                        \r
+            1. We do not refund or exchange to another item with a lower price after purchasing. `),S=o(n,"BR",{}),R=c(n,`\r
+                                            \r
+            2. We only support customers exchange the size of the same item or exchange to a new item if the item you received was damaged (photo and video for proving) in 3 days for online purchasing.\r
+                         `),T=o(n,"BR",{}),C=c(n,`                   \r
+            3. The item for exchange MUST BE returned in original condition with tags and labels included as well.\r
+                  `),I=o(n,"BR",{}),W=c(n,`                          \r
+            4. SALE items can not be refunded or exchanged. `),k=o(n,"BR",{}),A=c(n,`\r
+                        5. Customers must pay for the additional shipping fees for the exchanged items. Shipping fees are non-refundable.\r
+            `),N=o(n,"BR",{}),P=o(n,"BR",{}),z=c(n,`\r
+            \r
+            Thank you for your attention.`),n.forEach(p),F.forEach(p),q.forEach(p),L=M(i),H(d.$$.fragment,i),i.forEach(p),this.h()},h(){b(u,"class","title svelte-193tb0u"),b(t,"class","svelte-193tb0u"),b(f,"class","content svelte-193tb0u"),b(m,"class","container svelte-193tb0u"),b(r,"class","svelte-193tb0u")},m(s,i){te(s,r,i),O(h,r,null),e(r,y),e(r,u),e(u,w),e(r,_),e(r,m),e(m,f),e(f,t),e(t,x),e(t,$),e(t,E),e(t,B),e(t,S),e(t,R),e(t,T),e(t,C),e(t,I),e(t,W),e(t,k),e(t,A),e(t,N),e(t,P),e(t,z),e(r,L),O(d,r,null),v=!0},p:ne,i(s){v||(U(h.$$.fragment,s),U(d.$$.fragment,s),v=!0)},o(s){X(h.$$.fragment,s),X(d.$$.fragment,s),v=!1},d(s){s&&p(r),Y(h),Y(d)}}}class ae extends J{constructor(r){super(),K(this,r,null,re,Q,{})}}new ae({target:document.querySelector("#exchange")});
